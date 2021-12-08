9b9bd3d37e49bbe41dadee8e6bb6f7477f8b576d
Vitor Magno C. de Gouveia <45520686+VitorMagno@users.noreply.github.com>
2021-12-07 22:24:38 -0300

    add message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,7 +11,8 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
-  constructor(private heroService: HeroService) { }
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
   
   ngOnInit(): void {
     this.getHeroes();
@@ -23,12 +25,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+    this.messageService.add("HeroesComponent: selected hero id="+hero.id );
   }
 
-  heroes: Hero[] = [];
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
 
 }
